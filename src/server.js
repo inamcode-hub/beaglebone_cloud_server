f8db6eeacc9dc950ead1c64a82b70258826241bc
Inam Ul Rehman <inam@dryermaster.com>
2024-07-18 14:40:22 -0400

    refactor: Remove unnecessary commented code in index.js

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ const startServer = () => {
     });
   });
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 8080;
   server.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
   });
