52632af821372c285397f01b1ada73f2d9fe0929
Inam Ul Rehman <inam@dryermaster.com>
2024-09-05 10:38:51 -0400

    refactor: Update server.js to include express-rate-limit middleware for rate limiting requests

--- src/server.js
+++ src/server.js
@@ -11,23 +11,26 @@ const loggerMiddleware = require('./middlewares/loggerMiddleware');
 const errorHandler = require('./middlewares/errorHandler');
 const path = require('path');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit'); // Import rate limit
 
 const startServer = () => {
   const app = express();
   const server = http.createServer(app);
   const wss = new WebSocket.Server({ server });
 
+  // Enable trust proxy for rate limiting to correctly identify user IPs
+  app.set('trust proxy', true);
+
   // Apply rate limiting to all requests
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 100, // Limit each IP to 100 requests per `window` (15 minutes)
     message:
       'Too many requests from this IP, please try again after 15 minutes',
   });
 
-  app.use(cors());
-  app.use(limiter);
+  app.use(cors()); // Enable CORS
+  app.use(limiter); // Apply rate limiter
   app.use(express.json({ limit: '10mb' }));
   app.use(loggerMiddleware);
   app.use('/api', routes);
