13d17838eec9b776f3d6b524977ec163766cc70a
Inam Ul Rehman <inam@dryermaster.com>
2024-09-09 09:13:30 -0400

    refactor: Update rate limit to allow 500 requests per window

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ const startServer = () => {
   // Apply rate limiting to all requests
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (15 minutes)
+    max: 500, // Limit each IP to 100 requests per `window` (15 minutes)
     message:
       'Too many requests from this IP, please try again after 15 minutes',
   });
